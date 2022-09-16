f8d28648199a485114e25501006e137f7c9a1a48
asaelens02 <104400626+asaelens02@users.noreply.github.com>
2022-09-16 17:44:49 -0500

    declare starting variables in script.js

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+//declare global variables and retrieve as DOM elements
+
+var beginQuizPage = document.getElementById('beginquiz');
+var beginQuizbtn = document.getElementById('beginbutton');
+var quiz = document.getElementById('rockquiz');
+var quizTimer = document.getElementById ('timer');
+var quizQuestions = document.getElementById('questions');
+var aButton = document.getElementById ('a');
+var bButton = document.getElementById ('b');
+var cButton = document.getElementById ('c');
+var dButton = document.getElementById ('d');
+var resultsRecord = document.getElementById ('results');
+var score = document.getElementById ('inputscores');
+var finalScore= document.getElementById ('totalscore');
+var highScoreInitials = document. getElementById ('initials');
+var submitScoreBtn = document.getElementById('submit');
+var scoreContainer= document.getElementById('hscontainer');
+var highScorePage = document.getElementById('hspage');
+var showHighScore = document.getElementById('hsscore');
+var gameOverBtns = document.getElementById ('endbuttons');
+
+
+
+
+
+
+
+

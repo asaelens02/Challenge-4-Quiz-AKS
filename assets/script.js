18a666e521c5551f2a3f557e873503f5d7ce9e23
asaelens02 <104400626+asaelens02@users.noreply.github.com>
2022-09-16 18:06:57 -0500

    add question array to .js file

--- assets/script.js
+++ assets/script.js
@@ -19,6 +19,71 @@ var highScorePage = document.getElementById('hspage');
 var showHighScore = document.getElementById('hsscore');
 var gameOverBtns = document.getElementById ('endbuttons');
 
+//declare quiz question array
+
+var rockQuizQuestions =[ 
+    {
+        question: "What are the 3 types of rocks?",
+        answerA: "sedimentary, metamorphic, igneous",
+        answerB: "mineral, sedimentary,igneous",
+        answerC: "clastic, mineral, igneous",
+        answerD: "clastic, sedimentary, igneous",
+        rightAnswer: 'a'
+    },
+    {
+        question: "What mineral is also known as 'Fool's Gold'?",
+        answerA: "pyrite",
+        answerB: "quartz",
+        answerC: "calcite",
+        answerD: "dolomite",
+        rightAnswer:'a',
+    },
+    {
+        question: "How old is planet Earth?",
+        answerA: "4.6 billion years old",
+        answerB: "2.6 billion years old",
+        answerC: "0.6 billion years old",
+        answerD: "20,000 years old",
+        rightAnswer: 'a',
+    },
+    {
+        question: "What discipline encompasses the study of fossils?",
+        answerA: "paleontology",
+        answerB: "seismology",
+        answerC: "petrophysics",
+        answerD: "sedimentology",
+        rightAnswer: 'a',
+    },
+    {
+        questions: "What fossilized tree resin is commonly used in jewelry?",
+        answerA:"amber",
+        answerB:"maple",
+        answerC:"quartz",
+        answerD: "coprolite",
+        rightAnswer: 'a',
+
+    },
+    {
+        questions: "What are the 3 types of faults?",
+        answerA: "strike-slip, normal, reverse",
+        answerB: "convergent, divergent, strike-slip",
+        answerC: "strike-slip, normal, divergent",
+        answerD: "good, bad, ugly",
+        rightAnswer: 'a',
+    },
+    {
+        questions: "What dinosaur in the Original Jurassic Park caused the cup of water to ripple?",
+        answerA: "T-Rex",
+        answerB: "Blue the Raptor",
+        answerC: "Hadrosaur",
+        answerD: "Triceratops",
+        rightAnswer: 'a',
+    }
+
+]
+
+
+
 
 
 
